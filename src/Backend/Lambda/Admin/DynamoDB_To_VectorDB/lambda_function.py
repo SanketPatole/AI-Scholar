3455a6fe23e0faa0507070938cb4f902812a42a1
SanketPatole <69233883+SanketPatole@users.noreply.github.com>
2024-03-03 19:37:36 +0530

    Fixed bug in dynamodb to vectordb lambda

--- src/Backend/Lambda/Admin/DynamoDB_To_VectorDB/lambda_function.py
+++ src/Backend/Lambda/Admin/DynamoDB_To_VectorDB/lambda_function.py
@@ -1,3 +1,4 @@
+import logging
 import json
 import os
 import boto3
@@ -6,13 +7,17 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 
 def upload_directory_to_s3(s3_client, bucket_name, directory_path):
     for root, dirs, files in os.walk(directory_path):
         for file in files:
             local_path = os.path.join(root, file)
-            relative_path = os.path.relpath(local_path, directory_path)
-            s3_path = os.path.join(relative_path)
+            #relative_path = os.path.relpath(local_path, directory_path)
+            #s3_path = os.path.join(relative_path)
+            s3_path = os.path.join(local_path.replace('/tmp/', ''))
             s3_client.upload_file(local_path, bucket_name, s3_path)
 
 def get_splits(text):
@@ -25,6 +30,20 @@ def get_vectorstore(splits):
     vectorstore = FAISS.from_texts(splits, embedding=embedding)
     return vectorstore
 
+def process_entry(entry, s3, bucket_name):
+    try:
+        cls = entry['CLASS_SUBJECT']['S'].split('_')[0]
+        subject = entry['CLASS_SUBJECT']['S'].split('_')[1]
+        chapter = entry['CHAPTER']['S']
+        text = entry['CONTENT']['S']
+        splits = get_splits(text)
+        vectorstore = get_vectorstore(splits)
+        vectorstore.save_local(f"/tmp/{cls}/{subject}/{chapter}")
+        upload_directory_to_s3(s3, bucket_name, directory_path=f"/tmp/{cls}/{subject}/{chapter}")
+        logger.info(f"Loaded {cls} - {subject} - {chapter}")
+    except Exception as e:
+        logger.error(f"Error processing entry: {e}", exc_info=True)
+
 def lambda_handler(event, context):
     bucket_name = os.environ['BUCKET_NAME']
     tablename = os.environ['DYNAMODB_TABLE_NAME']
@@ -33,17 +52,18 @@ def lambda_handler(event, context):
     response = dynamodb.scan(TableName=tablename)
     data = response['Items']
     while response.get('LastEvaluatedKey'):
-        time.sleep(5)
-        response = dynamodb.scan(TableName=tablename,
-                ExclusiveStartKey=response['LastEvaluatedKey'])
+        time.sleep(3)
+        response = dynamodb.scan(TableName=tablename, ExclusiveStartKey=response['LastEvaluatedKey'])
         data.extend(response['Items'])
-    for entry in data:
-        cls = entry['CLASS_SUBJECT']['S'].split('_')[0]
-        subject = entry['CLASS_SUBJECT']['S'].split('_')[1]
-        chapter = entry['CHAPTER']['S']
-        text = entry['CONTENT']['S']
-        splits = get_splits(text)
-        vectorstore = get_vectorstore(splits[0])
-        vectorstore.save_local(f"/tmp/{cls}/{subject}/{chapter}")
-        print(f"Loaded {cls} - {subject} - {chapter}")
-    upload_directory_to_s3(s3, bucket_name, directory_path=f"/tmp/")
+    logger.info(f"There are {len(data)} records in dynamodb.")
+    
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        future_to_entry = {executor.submit(process_entry, entry, s3, bucket_name): entry for entry in data}
+        for future in as_completed(future_to_entry):
+            entry = future_to_entry[future]
+            try:
+                future.result()
+            except Exception as exc:
+                logger.error(f'Entry generated an exception: {exc}', exc_info=True)
+    
+    logger.info("All tasks completed.")
